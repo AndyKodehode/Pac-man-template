f10b069e2a5d5f605169dec7ed6e3b3131f05341
AndyKodehode <andykodehode@gmail.com>
2024-10-24 18:10:57 +0200

    utkast til veileder

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ container for grid
 "level" og noe som lagrer level
 */
 
+// css
+
+/*
+ Grids: høyde og bredde // hele rammen for spillet
+ Grids div: høyde og bredde // dette er boksene for vegger og pac-man
+ Flex-wrap på classen grid som får divs til å fylle hele grid
+ Lag en class for elementer på brettet, ghost skal ikke ha styling
+    0- Pac dots
+    1- wall
+    2- ghost layer
+    3- power-pellet
+    4- empty
+    pac-man
+*/
+
 // JS
 
 /*
@@ -48,5 +63,25 @@ const layout = [
     2- ghost layer
     3- power-pellet
     4- empty
+    
+
+    En loop som går igjennom hvert element i layout og lager divs for hvert element i layout
+    legg divs til grid
+    Lag en ny array utenfor loop, gi den et navn, push divs i loopen inn i den nye arrayen
+
+    Inne i loopen, lag sjekker som ser om indexen er et visst tall 
+    hvis det er tallet du ser etter, bruk array utenfor loop, med index, til å legge til det classenavnet som tallet representerer
+
+
+    Lag en startindex for pac-man, hvor han er på brettet, og bruk array fra tidligere med pacmanindex til å sette pac-man på brettet
+
+    finn ut hvilke keys du vil bruke for å flytte pac-man
+
+    finn en måte å fjerne class pac-man og legge til pac-man på en ny index
+
+    når man skal gå opp eller ned må man trekke fra eller legge til width for å få riktig index
+
+    modulus for å sjekke om man har truffet veggen
+
 
 */
